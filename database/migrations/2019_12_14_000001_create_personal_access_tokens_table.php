0d107c81a42f2e4be909765c540d2a0e81a0fd66
Fathur Walkers <fathurwalkers44@gmail.com>
2022-10-22 08:13:03 -0700

    update

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -6,21 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 class CreatePersonalAccessTokensTable extends Migration
 {
-    // public function up()
-    // {
-    //     Schema::create('personal_access_tokens', function (Blueprint $table) {
-    //         $table->id();
-    //         $table->morphs('tokenable');
-    //         $table->string('name');
-    //         $table->string('token', 64)->unique();
-    //         $table->text('abilities')->nullable();
-    //         $table->timestamp('last_used_at')->nullable();
-    //         $table->timestamps();
-    //     });
-    // }
+    public function up()
+    {
+        // Schema::create('personal_access_tokens', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->morphs('tokenable');
+        //     $table->string('name');
+        //     $table->string('token', 64)->unique();
+        //     $table->text('abilities')->nullable();
+        //     $table->timestamp('last_used_at')->nullable();
+        //     $table->timestamps();
+        // });
+    }
 
-    // public function down()
-    // {
-    //     Schema::dropIfExists('personal_access_tokens');
-    // }
+    public function down()
+    {
+        // Schema::dropIfExists('personal_access_tokens');
+    }
 }
